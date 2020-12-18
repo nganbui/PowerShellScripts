e18e1da132c8f017dad8efbc7fc735f71a8cc5be
Ngan Bui <ngan.bui@hotmail.com>
2020-12-18 14:27:38 -0500

    Add files via upload

---
+++ OD4B/GenerateSubCsvFiles.ps1
@@ -0,0 +1,14 @@
+﻿$OneDriveList = Import-Csv -LiteralPath "C:\TEMP\OneDrive\OneDrive.csv"
+
+$icList = @() #$OneDriveList | select IC | Get-Unique
+
+foreach($OneDrive in $OneDriveList)
+{
+    if($icList.Contains($OneDrive.IC) -eq $false)
+    {
+        $icList += $OneDrive.IC
+        $icOneDrives = $OneDriveList | ? {$_.IC -eq $OneDrive.IC} 
+        $icOneDrives | Export-Csv -LiteralPath "C:\TEMP\OneDrive\$($OneDrive.IC).csv" -NoTypeInformation
+    }
+}
+
