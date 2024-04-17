562792247fd507796aa3194f5813900f20870021
Ngan Bui <ngan.bui@hotmail.com>
2024-04-17 12:02:15 -0400

    Update ImportPFXCertPersonalCertStore.ps1

--- AdminPortal-Operations/ImportPFXCertPersonalCertStore.ps1
+++ AdminPortal-Operations/ImportPFXCertPersonalCertStore.ps1
@@ -1,4 +1,3 @@
-﻿$CertificateFilePath = '\\nihspodev\d$\Scripting\O365DevOps\AdminScripts\UnitTests\SPO-ExchangeOnlinePowerShellV2.pfx'
 ## Get the PFX password
 $mypwd = Get-Credential -UserName 'Enter password below' -Message 'Enter password below'
 ## Import the PFX certificate to the current user's personal certificate store.
